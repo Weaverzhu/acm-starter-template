0b79696347515f5783eaf0da0d668fb80457c09c
weaver_zhu <weaver_zhu@outlook.com>
2019-07-18 11:22:28 +0800

    0718

--- main.cpp
+++ main.cpp
@@ -14,34 +14,78 @@ void err(T a, A... x) { cout << a << ' '; err(x...); }
 #define dbg(...)
 #endif
 
-struct ios;
-ios io;
 
 //--------------------------------------------------------------------------------------------
 
+typedef __int128 LLL;
+
+LL n;
+
+namespace lgcd {
+    LLL f(LLL a, LLL b, LLL c, LLL n) {
+        LLL res = 0;
+        if (n <= 5) {
+            for (int i=0; i<=n; ++i) {
+                res += (a*i+b)/c;
+            }
+        } else if (a >= c || b >= c) {
+            res = (a/c) * (n*(n+1)/2) + (b/c)*(n+1) + f(a%c, b%c, c, n);
+        } else {
+            LLL m = (a*n+b)/c;
+            res = n * m - f(c, c-b-1, a, m-1);
+        }
+        return res;
+    }
+}
+
+LLL g(LLL n, LLL p, LLL q) {
+    LLL a = p, b = 0, c = q;
+    return lgcd::f(a, b, c, n);
+}
+
+LLL cnt(LLL n, LLL p1, LLL q1, LLL p2, LLL q2) {
+    return g(n, p1, q1) - g(n, p2, q2);
+}
 
 
+void solve() {
+    char input[100];
+    scanf("%s", input);
+    sscanf(input, "0.%lld", &n);
+    LLL p1 = n, p2 = n, q1 = 1, q2 = q1, p = n * 10;
+    for (int i=0; i<19; ++i) {
+        q1 *= 10;
+        q2 *= 10;
+    }
+    p1 = p1 * 10 + 5;
+    p2 = p2 * 10 - 5;
 
+    LLL l = 0, r = 1e9+5;
+    while (l < r-1) {
+        LLL m = (l+r) >> 1;
+        // print(m);
+        if (cnt(m, p1, q1, p2, q2) < 1)
+            l = m;
+        else 
+            r = m;
+    }
+    LLL x = p * r / q1, y = r, g = __gcd(x, y);
+    x /= g;
+    y /= g;
+    LL xx = x, yy = y, gg = __gcd(xx, yy);
+    xx /= gg;
+    yy /= gg;
+    // printf("%lld %lld\n", xx, yy);
+    cout << xx << ' ' << yy << endl;
+}
 
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int t;
+    scanf("%d", &t); getchar();
+    for (int kk=0; kk<t; ++kk)
+        solve();
     return 0;
 }
 
-
-struct ios {
-    inline char gc(){
-        static const int IN_LEN=1<<18|1;
-        static char buf[IN_LEN],*s,*t;
-        return (s==t)&&(t=(s=buf)+fread(buf,1,IN_LEN,stdin)),s==t?-1:*s++;
-    }
-
-    template <typename _Tp> inline ios & operator >> (_Tp&x){
-        static char ch,sgn; ch = gc(), sgn = 0;
-        for(;!isdigit(ch);ch=gc()){if(ch==-1)return *this;sgn|=ch=='-';}
-        for(x=0;isdigit(ch);ch=gc())x=x*10+(ch^'0');
-        sgn&&(x=-x); return *this;
-    }
-} io;
