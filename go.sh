365d1c42ae3a1b40120be5cf07922e653acf0db9
weaver_zhu <weaver_zhu@outlook.com>
2019-08-10 20:37:54 +0800

    add compile

--- go.sh
+++ go.sh
@@ -36,17 +36,22 @@ elif [ $1 = "gen" ]; then
     compile "./judger/generator.cpp" "./bin/gen"
     run "./bin/gen" "./input/in.txt" "./input/in.txt"
 elif [ $1 = "judge" ]; then
-    compile "./main.cpp" "./bin/main"
-    run "./bin/main" "./input/in.txt" "./output/out"
-    compile "./judger/std.cpp" "./bin/std"
-    run "./bin/std" "./input/in.txt" "./output/std"
-    if diff "./output/out" "./output/std"; then
-        echo Verdict: AC
-    else
-        echo Verdict: WA
-    fi
+    while true; do
+        compile "./main.cpp" "./bin/main"
+        run "./bin/main" "./input/in.txt" "./output/out"
+        compile "./judger/std.cpp" "./bin/std"
+        run "./bin/std" "./input/in.txt" "./output/std"
+        if diff "./output/out" "./output/std"; then
+            echo Verdict: AC
+        else
+            echo Verdict: WA
+            exit 0
+        fi
+    done
 elif [ $1 = "newf" ]; then
     run "./bin/starter" "./judger/starter.txt" "./main.cpp"
+elif [ $1 = "compile" ]; then
+    compile "./main.cpp" "./bin/main"
 else
     echo log: wrong input
 fi
