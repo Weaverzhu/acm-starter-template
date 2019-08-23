bd9266a2c80c78299c59ef5d21fa787b009c26eb
18级 <10185102128@cslab.ecnu.edu.cn>
2019-08-23 11:47:05 +0800

    test

--- go.sh
+++ go.sh
@@ -36,7 +36,9 @@ elif [ $1 = "gen" ]; then
     compile "./judger/generator.cpp" "./bin/gen"
     run "./bin/gen" "./input/in.txt" "./input/in.txt"
 elif [ $1 = "judge" ]; then
+    compile "./judger/generator.cpp" "./bin/gen"
     while true; do
+        run "./bin/gen" "./input/in.txt" "./input/in.txt"
         compile "./main.cpp" "./bin/main"
         run "./bin/main" "./input/in.txt" "./output/out"
         compile "./judger/std.cpp" "./bin/std"
@@ -52,6 +54,16 @@ elif [ $1 = "newf" ]; then
     run "./bin/starter" "./judger/starter.txt" "./main.cpp"
 elif [ $1 = "compile" ]; then
     compile "./main.cpp" "./bin/main"
+elif [ $1 = "test" ]; then
+    compile "./main.cpp" "./bin/main"
+    run "./bin/main" "./input/in.txt" "./output/out"
+    compile "./judger/std.cpp" "./bin/std"
+    run "./bin/std" "./input/in.txt" "./output/std"
+    if diff "./output/out" "./output/std"; then
+        echo Verdict: AC
+    else
+        echo Verdict: WA
+    fi
 else
     echo log: wrong input
 fi
