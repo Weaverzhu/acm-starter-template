22598cbf683a2ac4d1120322f520a310d4a8bec5
Weaverzhu <weaver_zhu@outlook.com>
2019-11-19 13:20:00 +0800

    v2.1

--- go.sh
+++ go.sh
@@ -10,27 +10,27 @@ echo .
 if [ $# -eq 1 ]; then
     if [ $1 = "gen" ]; then
         echo log: generating input data
-        make gen
-        ./gen > ./input/in.txt
+        make ./bin/gen
+        ./bin/gen > ./input/in.txt
     elif [ $1 = "compile" ]; then
         echo log: just compile main.cpp
-        make
+        make ./bin/main
     elif [ $1 = "stdin" ]; then
         echo log: run from stdin
-        make
+        make ./bin/main
         echo ------------- output --------------------
-        ./main
+        ./bin/main
     elif [ $1 = "judge" ]; then
         echo 'log: judge with huge test cases'
-        make
-        make std
-        make gen
+        make ./bin/main
+        make ./bin/std
+        make ./bin/gen
         echo 'compile completed...'
 
         while true; do
-            ./gen > ./input/in.txt
-            ./main < ./input/in.txt > ./output/out.txt
-            ./std < ./input/in.txt > ./output/std.txt
+            ./bin/gen > ./input/in.txt
+            ./bin/main < ./input/in.txt > ./output/out.txt
+            ./bin/std < ./input/in.txt > ./output/std.txt
             if diff "./output/out" "./output/std"; then
                 echo Verdict: AC
             else
@@ -40,31 +40,31 @@ if [ $# -eq 1 ]; then
         done
     elif [ $1 = "newf" ]; then
         echo log: reset main.cpp
-        cat ./judger/starter.txt > ./main.cpp
+        cat ./judger/starter.txt > ./bin/main.cpp
     elif [ $1 = "test" ]; then
         echo 'log: run a single test case'
-        make
-        make std
-        make gen
+        make ./bin/main
+        make ./bin/std
+        make ./bin/gen
         echo 'compile completed...'
 
-        ./gen > ./input/in.txt
-        time ./main < ./input/in.txt > ./output/out.txt
-        time ./std < ./input/in.txt > ./output/std.txt
+        ./bin/gen > ./input/in.txt
+        time ./bin/main < ./input/in.txt > ./output/out.txt
+        time ./bin/std < ./input/in.txt > ./output/std.txt
         if diff "./output/out" "./output/std"; then
             echo Verdict: AC
         else
             echo Verdict: WA
         fi
     elif [ $1 = "python" ]; then
-        python3 "./main.py" < "./input/in.txt"
+        python3 "./bin/main.py" < "./input/in.txt"
     else
         echo log: wrong input
     fi
 elif [ $# -eq 0 ]; then
-    make
+    make ./bin/main
     echo ------------- output --------------------
-    time ./main < ./input/in.txt
+    time ./bin/main < ./input/in.txt
 else
     echo 'Usage: bash go.sh <command>'
 fi
--- makefile
+++ makefile
@@ -1,13 +1,11 @@
-main: main.cpp
-	g++ main.cpp -o ./main -std=c++11 -fsanitize=address -Wall
-mainO2: main.cpp
-	g++ main.cpp -o ./main -std=c++11 -fsanitize=address -mainO2 -Wall
+./bin/main: main.cpp
+	g++ main.cpp -o ./bin/main -std=c++11 -fsanitize=address -Wall
 clean:
 	rm -f main
 	rm -f gen
 	rm -f std
 	rm -f ./output/*
-gen: ./judger/generator.cpp
-	g++ ./judger/generator.cpp -o ./gen -O2 -std=c++11 -fsanitize=address
-std: ./judger/std.cpp
-	g++ ./judger/std.cpp -o ./gen -O2 -std=c++11 -fsanitize=address
+./bin/gen: ./judger/generator.cpp
+	g++ ./judger/generator.cpp -o ./bin/gen -O2 -std=c++11 -fsanitize=address
+./bin/std: ./judger/std.cpp
+	g++ ./judger/std.cpp -o ./bin/std -O2 -std=c++11 -fsanitize=address
